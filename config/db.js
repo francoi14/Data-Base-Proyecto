d20bd78692f20b4175251a9f71aee94d02c4c62e
Franco illanes <Franco.illanes123@gmail.com>
2026-07-01 09:09:01 -0300

    Actualizar engines y dependencias

--- config/db.js
+++ config/db.js
@@ -2,12 +2,12 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
     try {
-        // Conexión a MongoDB usando la URI del archivo .env
+        // ConexiOn a MongoDB usando la URI del archivo .env
         await mongoose.connect(process.env.MONGO_URI);
         console.log('✅ MongoDB Conectado: El kiosco está online');
     } catch (err) {
         console.error('❌ Error de conexión:', err.message);
-        process.exit(1); // Cierra el proceso si falla la conexión
+        process.exit(1); // Cierra el proceso si falla la conexiOn
     }
 };
 
